e3f6744ca4a002132906fe1836485eb94c643f8d
Lugin Dmitriy <lugin.dmitriy@yandex.ru>
2020-05-16 02:42:01 +0300

    fix for IE

--- helpers/deviceUseCursor.js
+++ helpers/deviceUseCursor.js
@@ -1,9 +1,10 @@
+import { browserIsIE } from './browserIsIE'
 /**
  * Check user devise use cursor(mouse) or not
  * @return {Boolean}
  */
 function deviceUseCursor() {
-    return window.matchMedia('(hover:hover)').matches
+    return window.matchMedia('(hover:hover)').matches || browserIsIE()
 }
 
 export { deviceUseCursor }
